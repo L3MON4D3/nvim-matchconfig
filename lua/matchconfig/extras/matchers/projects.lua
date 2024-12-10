a73d1e91d815998fdcab911b35277093d5ae0812
L3MON4D3 <Simljk@outlook.de>
2024-12-10 15:32:36 +0100

    slightly rework framework-matchers to project-matchers.
    That name seems more appropriate.

--- lua/matchconfig/extras/matchers/projects.lua
+++ lua/matchconfig/extras/matchers/projects.lua
@@ -1,14 +1,28 @@
-local framework_id_fns = {
+local project_id_fns = {
 	cmake = function(bufinfo)
-		return #vim.fs.find("CMakeLists.txt", {upward=true, type="file", limit=1, path = bufinfo.dir})
+		-- return directory of uppermost CMakeLists.txt, that's most likely the correct one.
+		-- 16 should be a reasonable upper limit.
+		local res = vim.fs.find("CMakeLists.txt", {upward=true, type="file", limit=16, path = bufinfo.dir})
+		return #res > 0 and vim.fs.dirname(res[#res])
+	end,
+	pkgbuild = function(bufinfo)
+		-- there shouldn't be nested PKGBUILDS.
+		local res = vim.fs.find("PKGBUILD", {upward=true, type="file", limit=1, path = bufinfo.dir})
+		return #res > 0 and vim.fs.dirname(res[1])
+	end,
+	make = function(bufinfo)
+		-- return directory of uppermost Makefile, that's most likely the correct one.
+		-- 16 should be a reasonable upper limit.
+		local res = vim.fs.find("Makefile", {upward=true, type="file", limit=16, path = bufinfo.dir})
+		return #res > 0 and vim.fs.dirname(res[#res])
 	end,
 }
 
 local M = {}
 
-for framework, id_fn in pairs(framework_id_fns) do
-	M[framework] = function()
-		return require("matchconfig.builtin_matchers").generic.new(id_fn, framework, "framework")
+for k, id_fn in pairs(project_id_fns) do
+	M[k] = function()
+		return require("matchconfig.builtin_matchers").generic.new(id_fn, k, "project")
 	end
 end
 
